52a02ee13eeeef5391bcff7d6a649a66d6039bbb
Adriel <adriel.fariasan@gmail.com>
2022-07-10 23:17:30 -0300

    Implementa controller de mensagem

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -1,5 +1,16 @@
-function message(request, response) {
-  response.send(request.body);
+import * as MessageService from '../services/message.js';
+
+async function message(request, response) {
+  const { message, phoneNumber } = request.body;
+  try {
+    const responseMessage = await MessageService.sendMessage(
+      message,
+      phoneNumber
+    );
+    response.send(responseMessage);
+  } catch (error) {
+    response.status(error.status).send(error);
+  }
 }
 
 export { message };
